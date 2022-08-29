8d560a4424ea9d111d61d9371f7b90ea93cb92fc
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-08-30 01:14:16 +0200

    done task -1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categories = document.querySelectorAll(".item");
+const numberOfCategories = categories.length;
+console.log(`Number of categories: ${numberOfCategories}`);
+categories.forEach((element) => {
+        console.log(`Category: ${element.firstElementChild.textContent}`),
+          console.log(`Elements: ${element.lastElementChild.children.length}`);
+    }
+);
