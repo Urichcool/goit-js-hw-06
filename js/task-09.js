ad860979836b1d8c3eaca87725962328a1219b9a
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-09-04 23:32:29 +0200

    done task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const buttonEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+buttonEl.addEventListener("click", event => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+});
