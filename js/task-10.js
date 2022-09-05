80fbd0dfdf8f9f9ece69dfac4def9ff3feff196e
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-09-05 15:50:52 +0200

    task 10 -in progress

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,8 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const controlEl = document.querySelector("#controls");
+const boxesEl = document.querySelector("#boxes")
+const createButoonEl = document.querySelector("[data-create]");
+const destroyButoonEl = document.querySelector("[data-destroy]");
+createButoonEl.addEventListener("click",);
