b8aef9787fe744356be441346c3e2d192d1fae00
Urichcool <104870932+Urichcool@users.noreply.github.com>
2022-09-06 00:07:35 +0200

    done task 10

--- js/task-10.js
+++ js/task-10.js
@@ -7,10 +7,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
-function getAmount() {
-  
-  createBoxes(amount);
-};
 
 function createBoxes(amount) {
   const newArr = [];
@@ -24,7 +20,7 @@ function createBoxes(amount) {
 
 function destroyBoxes() {
   boxesEl.innerHTML = "";
-}
+};
 
 
 createButtonEl.addEventListener("click", (event) => {
